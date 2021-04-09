4cf82dda3f501b79dd2e7393466a9e28cc2e61b0
wilsondsn <info@wilsondsn.cl>
2021-04-08 23:12:21 -0400

    Se creó la clase .negro para añadir color de fondo a los contenedores. Menú pasa de transparente a negro al hacer scroll. Se actualizó la función addClass asignada al menú

--- prueba_final/assets/js/script.js
+++ prueba_final/assets/js/script.js
@@ -4,13 +4,9 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 	var menu = $("nav");
     if (scroll >= 200) {
-        menu.addClass("navbar-light");
-        menu.addClass("bg-light");
-        menu.removeClass("navbar-dark")
+        menu.addClass("negro");
     } else {
-        menu.removeClass("navbar-light");
-        menu.removeClass("bg-light");
-        menu.addClass("navbar-dark")
+        menu.removeClass("negro");
     }
 });
 
