3af35c4fbd28b18b585db0e524ac09196da51fd9
wilsondsn <info@wilsondsn.cl>
2021-04-09 00:44:43 -0400

    Se aplicaron los estilos css definidos. y se eliminó un error en el código js relacionado al smooth scroll

--- prueba_final/assets/js/script.js
+++ prueba_final/assets/js/script.js
@@ -1,5 +1,6 @@
 $(function() {
 
+// Añadir clase al nav al hacer scroll
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 	var menu = $("nav");
@@ -9,5 +10,25 @@ $(window).scroll(function() {
         menu.removeClass("negro");
     }
 });
+
+// Añadir clase .active al elemento del menú seleccionado y hacer scroll
+$('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+         var target = this.hash,
+         menu = target;
+        $target = $(target);
+              
+       $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+1
+        }, 600, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 
 });
