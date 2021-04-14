e9f5c97f4649250372e76f46b381b2d892d61b5a
wilsondsn <info@wilsondsn.cl>
2021-04-13 22:22:35 -0400

    Se añadieron comentarios de documentación a index.html. Se añadieron estilos personalizados a Tooltips. Se justaron comentarios en archivo js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(function() {
     // Añade la clase .active a cada item según se transite por cada uno.
     $('body').scrollspy({ target: '#navbarNav' })
 
-        // Añadir clase al nav al hacer scroll
+    // Añadir clase al nav al hacer scroll
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         var menu = $("nav");
@@ -15,35 +15,34 @@ $(function() {
         }
     });
 
-    // Añadir clase .active al elemento del menú seleccionado y hacer scroll
-
-    $("a").click(function(event) { // seleccionamos la etiqueta a y un clic como función del evento
+    // Añadir clase .active al elemento del menú seleccionado y hacer scroll hacia el mismo
+    
+    $("a").click(function(event) { 
         // alert(this.hash);
         if (this.hash !== "") {
             event.preventDefault();
 
             var gato = this.hash;
 
-            $('a').each(function() { // al hacer clic en un link, remover la clase active de otro que la tenga.
+            $('a').each(function() { 
                 $(this).removeClass('active');
             })
-            $(this).addClass('active'); // al hacer clic en un link, añadir a éste la clase active
+            $(this).addClass('active'); 
             var target = this.hash,
                 menu = target;
             $target = $(target);
 
             $("html, body").animate({
                 scrollTop: $(gato).offset().top
-            }, 800, function() { // 800 milisegundos
-                window.location.hash = gato; // busca la locación de la variable gato.
+            }, 800, function() { 
+                window.location.hash = gato; 
            });
         };
 
     });
 
 
-
-    // Activa Tooltips
+    // BootstrapJS: Activa Tooltips
     $(function() {
         $('[data-toggle="tooltip"]').tooltip()
     });
